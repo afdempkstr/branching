8b9ebc04540c5276a8992e89f2277558167c7b10
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2018-11-21 20:49:50 +0200

    Cleanup usings

--- branching/Student.cs
+++ branching/Student.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace branching
+﻿namespace branching
 {
     public class Student
     {
-        public string Name { get; set; }
+        public string Name { get; }
 
         public Student(string name)
         {
